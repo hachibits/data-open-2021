a31f2b1062dd26128ca061e948022ce97d478d2b
Oliver Xu <olxu@hotmail.com>
2021-07-16 21:48:00 +1000

    updated parser to now write to db

---
+++ load_db.py
@@ -0,0 +1,58 @@
+import psycopg2
+import argparse
+import os
+import pandas as pd
+
+def loadDatabase(conn, path, schema):
+    c = conn.cursor()
+    df =  pd.read_csv(path, compression='gzip', index_col=0) if path[-3:] == ".gz" else pd.read_csv(path, index_col=0)
+    for i, row in df.iterrows():
+        c.execute("""
+          INSERT INTO %(schema)s VALUES(
+            %(row)s
+          );
+        """ % {
+            "schema" : schema,
+            "row" : ", ".join(map(str, row))
+        })
+
+    conn.commit()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument("-h", "--host", nargs='?', action="store", dest="host", default="localhost")
+    parser.add_argument("-p", "--port", nargs='?', action="store", dest="port", default="5432")
+    parser.add_argument("-d", "--dbname", nargs='?', action="store", dest="dbname", default="postgres")
+    parser.add_argument("-U", "--username", nargs='?', action="store", dest="username", default="postgres")
+    #parser.add_argument("-w", "--no-password", action="store_true")
+    parser.add_argument("-W", "--password", nargs='?', action="store", dest="password", default=None)
+    parser.add_argument("--listings", action="store_true", default=False)
+    parser.add_argument("--calendar", action="store_true", default=False)
+    parser.add_argument("--real_estate", action="store_true", default=False)
+    parser.add_argument("--venues", action="store_true", default=False)
+
+    args = parser.parse_args()
+
+    conn = psycopg2.connect(
+        host = args.host,
+        port = args.port,
+        database = args.dbname,
+        user = args.username,
+        password = args.password
+    )
+
+    path = "./data"
+    if args.listings:
+        listings_path = os.path.join(path, "listings.csv")
+        loadDatabase(conn, listings_path, "listings")
+    if args.calendar:
+        calendar_path = os.path.join(path, "calendar.csv.gz")
+        loadDatabase(conn, calendar_path, "calendar")
+    if args.real_estate:
+        real_estate_path = os.path.join(path, "real_estate.csv.gz")
+        loadDatabase(conn, real_estate_path, "real_estate")
+    if args.venues:
+        venues_path = os.path.join(path, "venues.csv.gz")
+        loadDatabase(conn, venues_path, "venues")
+
+    conn.close()
